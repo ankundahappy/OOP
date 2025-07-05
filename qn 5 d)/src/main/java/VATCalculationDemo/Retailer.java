891f953115b9ee39959b21e76f2256139e59c9c4
ankundahappy <ankundahappy53@gmail.com>
2025-07-05 15:31:44 +0300

    Add files via upload

---
+++ qn 5 d)/src/main/java/VATCalculationDemo/Retailer.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VATCalculationDemo;
+ class Retailer extends vatcalulationdemo.TaxCategory {
+        @Override
+        public double calculateVAT(double amount) {
+            return amount * 0.18; // 18% VAT
+        }
+    }
